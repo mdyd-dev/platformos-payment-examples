c7f2f44bb3a8684c8145093e45ff64fa790689eb
Jacek Norbert <jaceknorbert@iMac-Jacek.local>
2019-04-23 17:38:05 +0200

    Update of necessary data

--- modules/account_example/test/page-object.js
+++ modules/account_example/test/page-object.js
@@ -8,12 +8,18 @@ export default class Stripe {
     this.button = {
       submit: Selector('button[type="submit"]'),
       addAccount: Selector('a[data-add-account]'),
-      deleteAccount: Selector('.delete-stripe')
+      deleteAccount: Selector('.delete-stripe'),
     };
     this.iframe = {
       iframeStripe: Selector('iframe[name="stripe_checkout_app"]'),
       validation: Selector('.Popover-content')
     };
+    this.link = {
+      editAccount: Selector('a').withText('Edit')
+    }
+    this.element = {
+      address: Selector('.mt-3 li').withText('Address')
+    }
     this.input = {
       country: Selector['data-country'],
       routingNumber: Selector('[data-routing-number]'),
@@ -24,6 +30,11 @@ export default class Stripe {
       dateOfBirth: Selector('[data-date-of-birth]'),
       email: Selector('input[type="email"]'),
       password: Selector('input[type="password"]'),
+      address: Selector('.person.border-bottom.border-light.mb-5 #input-address'),
+      city: Selector('.person.border-bottom.border-light.mb-5 #input-city'),
+      state: Selector('.person.border-bottom.border-light.mb-5 #input-state'),
+      code: Selector('.person.border-bottom.border-light.mb-5 #input-zip'),
+
       cardNumber: Selector(
         '.Fieldset-childLeftRight > .Textbox-inputRow > input[type="tel"].Fieldset-input.Textbox-control'
       ),
