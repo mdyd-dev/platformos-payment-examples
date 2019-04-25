8b1b325e18fc59b333f6d5e9f5763cd0812ba513
Jacek Norbert <jaceknorbert@iMac-Jacek.local>
2019-04-25 10:41:58 +0200

    Refactoring

--- modules/customer_example/test/index.js
+++ modules/customer_example/test/index.js
@@ -5,28 +5,20 @@ import faker from 'faker';
 import Stripe from './page-object';
 import Login from '../../../tests/e2e/page-objects/login';
 import {
+  credit_card,
+  user_stripe
+} from '../../../tests/e2e/data/data.js';
+import {
   getBtAlertText
 } from '@platform-os/testcafe-helpers';
 
 const stripe = new Stripe();
 const login = new Login();
 
-const {
-  email,
-  password
-} = {
-  email: 'test_stripe@test.com',
-  password: 'password'
-};
-
-const VALID_CC = '4242 4242 4242 4242';
-const INVALID_CC = '4000 0000 0000 0002';
-const VALID_NO_CHARGE = '4000 0000 0000 0341';
-
 fixture('Stripe')
   .page(process.env.MP_URL)
   .beforeEach(async t => {
-    await login.login(email, password);
+    await login.login(user_stripe.email, user_stripe.password);
     await t.navigateTo('/customer');
   });
 
@@ -37,7 +29,7 @@ test('Add new customer with credit card', async t => {
   await t
     .click(stripe.button.submit)
     .switchToIframe(stripe.iframe.iframeStripe)
-    .typeText(stripe.input.cardNumber, VALID_CC)
+    .typeText(stripe.input.cardNumber, credit_card.VALID_CC)
     .typeText(stripe.input.date, '12/23')
     .typeText(stripe.input.ccv, '111')
     .click(stripe.button.submitCharge)
@@ -68,7 +60,7 @@ test('Add new customer with invalid credit card', async t => {
   await t
     .click(stripe.button.submit)
     .switchToIframe(stripe.iframe.iframeStripe)
-    .typeText(stripe.input.cardNumber, VALID_NO_CHARGE)
+    .typeText(stripe.input.cardNumber, credit_card.VALID_NO_CHARGE)
     .typeText(stripe.input.date, '12/23')
     .typeText(stripe.input.ccv, '111')
     .click(stripe.button.submitCharge)
